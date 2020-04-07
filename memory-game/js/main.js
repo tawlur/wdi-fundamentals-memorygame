2a95432fb6163ed0133b7177f977891f7ab5d17d
Macbook Pro <macbookpro@Tawlur-MBP.hsd1.co.comcast.net>
2020-04-07 16:58:39 -0600

    created game logic

---
+++ memory-game/js/main.js
@@ -0,0 +1,8 @@
+
+let cardOne = "queen";
+let cardTwo = "king";
+let cardThree = "queen";
+let cardFour = "queen";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree);
