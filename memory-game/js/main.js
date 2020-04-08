743b1744d760f3fe6098f43262dfd3773155f357
Macbook Pro <macbookpro@Tawlur-MBP.hsd1.co.comcast.net>
2020-04-08 17:20:18 -0600

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,8 +1,27 @@
 
-let cardOne = "queen";
-let cardTwo = "king";
-let cardThree = "queen";
-let cardFour = "queen";
+let cards = ["queen", "queen", "king", "king"];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+	let cardsInPlay = [];
+
+	var cardOne = cards[0];
+
+		cardsInPlay.push(cardOne);
+
+			var cardOne = "User flipped queen";
+
+			console.log(cardOne);
+
+
+	var cardTwo = cards[2];
+
+		cardsInPlay.push(cardTwo);
+
+			var cardTwo = "User flipped king";
+
+			console.log(cardTwo);
+
+if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("you found a match!")
+} else {
+	alert("Sorry, try again.")
+}
