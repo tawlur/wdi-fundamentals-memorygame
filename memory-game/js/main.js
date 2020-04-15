a178c9c7566e0752215b883765cf840064bad84d
Macbook Pro <macbookpro@Tawlur-MBP.hsd1.co.comcast.net>
2020-04-15 11:50:20 -0600

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,27 +1,52 @@
 
-let cards = ["queen", "queen", "king", "king"];
-
-	let cardsInPlay = [];
-
-	var cardOne = cards[0];
-
-		cardsInPlay.push(cardOne);
-
-			var cardOne = "User flipped queen";
-
-			console.log(cardOne);
-
-
-	var cardTwo = cards[2];
-
-		cardsInPlay.push(cardTwo);
-
-			var cardTwo = "User flipped king";
-
-			console.log(cardTwo);
-
-if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("you found a match!")
-} else {
-	alert("Sorry, try again.")
+let cards = [
+{
+rank: 'queen',
+suit: 'hearts',
+cardImage: "images/queen-of-hearts.png"
+},
+{
+rank: 'queen',
+suit: 'diamonds',
+cardImage: "images/queen-of-diamonds"
+},
+{
+rank: 'King',
+suit: 'hearts',
+cardImage: "images/king-of-hearts.png"
+},
+{
+rank: 'king',
+suit: 'diamonds',
+cardImage: "images/king-of-diamonds.png"
 }
+];
+
+var cardsInPlay = [];
+
+var checkForMatch = function() {
+
+	if(cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.")
+	}
+}
+
+
+var flipCard = function () {
+
+	
+	cardsInPlay.push(cards[cardID].rank);
+
+	console.log(cards[cardID].suit);
+	console.log(cards[cardId].cardImage);
+}
+	
+
+
+//creat a function that will store all stpes related to seletin or flipping ove a macrd
+ 
+
+
+//second create a function to grpup together the logice to check to see if the two cards that flipped over matcheach other.
